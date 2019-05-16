0bd1474cbd9aa7262305a6ff098e1fb0c0932565
MaxHuw <m.huw.graham@gmail.com>
2019-05-16 15:42:39 -0400

    Basic logout deletes tokens and redirects.

--- client/src/components/navigation/Header.js
+++ client/src/components/navigation/Header.js
@@ -1,7 +1,34 @@
+//NPM
+import jwt from 'jsonwebtoken';
+
+//React
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 class Header extends Component {
 
+  logout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    this.props.history.push("/login");
+  }
+
+  render(){
+
+    let tokenData = jwt.decode(localStorage.getItem('accessToken'))
+
+    return(
+      <div>
+        <div>
+          {`Welcome ${tokenData.firstName}`}
+        </div>
+        <button 
+            onClick={this.logout}>
+            Logout
+        </button> 
+      </div>
+    )
+  }
 }
 
-export default Header;
+export default withRouter (Header);
